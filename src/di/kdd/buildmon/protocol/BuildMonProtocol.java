14b48609f4edc453e7c8bdac4529e5ded97351ec
Panos Sakkos <panos.sakkos@gmail.com>
2013-03-13 16:16:19 +0200

    Added Protocol class and interface

--- src/di/kdd/buildmon/protocol/BuildMonProtocol.java
+++ src/di/kdd/buildmon/protocol/BuildMonProtocol.java
@@ -2,4 +2,10 @@ package di.kdd.buildmon.protocol;
 
 public class BuildMonProtocol implements IProtocol {
 
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/di/kdd/buildmon/protocol/IProtocol.java
+++ src/di/kdd/buildmon/protocol/IProtocol.java
@@ -1,5 +1,18 @@
 package di.kdd.buildmon.protocol;
 
 public interface IProtocol {
+
+	static final int PORT_NUMBER = 4632;
 	
+	/* Message tags */
+	
+	static final String KNOCK_KNOCK_TAG = "KNOCK";
+	static final String HEARTBEAT_TAG = "HEARTBEAT";
+	static final String GET_PEAKS = "GET_PEAKS";
+	
+	/***
+	 * Entry point of the protocol.
+	 */
+	
+	void start();
 }
