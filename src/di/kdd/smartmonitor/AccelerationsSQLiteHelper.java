8733a7f161b8445e834638be12012692d65de486
Panos Sakkos <panos.sakkos@gmail.com>
2013-04-01 18:42:17 +0300

    removed unused import

--- src/di/kdd/smartmonitor/AccelerationsSQLiteHelper.java
+++ src/di/kdd/smartmonitor/AccelerationsSQLiteHelper.java
@@ -2,7 +2,6 @@ package di.kdd.smartmonitor;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
