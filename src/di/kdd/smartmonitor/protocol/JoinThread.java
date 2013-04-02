95b6a574cd7ddecaaa50b391041b8f90a0a12aa2
Panos Sakkos <panos.sakkos@gmail.com>
2013-04-02 16:45:51 +0300

    made join listener persistent

--- src/di/kdd/smartmonitor/protocol/JoinThread.java
+++ src/di/kdd/smartmonitor/protocol/JoinThread.java
@@ -10,9 +10,8 @@ import di.kdd.smartmonitor.protocol.IProtocol.Tag;
 
 public class JoinThread extends Thread {
 	private PeerData peerData;
-	private ServerSocket welcomeSocket;
 	
-	private static final String TAG = "knock knock listener";
+	private static final String TAG = "join listener";
 	
 	public JoinThread(PeerData peerData) {
 		this.peerData = peerData;
@@ -21,42 +20,44 @@ public class JoinThread extends Thread {
 	@Override
 	public void run() {
 		Message message;
+		ServerSocket joinSocket;
 				
 		android.os.Debug.waitForDebugger();
 		
 		try {
-			welcomeSocket = new ServerSocket(IProtocol.JOIN_PORT);
+			joinSocket = new ServerSocket(IProtocol.JOIN_PORT);
 		} 
 		catch (IOException e) {
 			Log.e(TAG, "Could not bind socket at the knock knock port");
 			e.printStackTrace();
+			
+			return;
 		}
 
-		try {
-			while(true) {
-					Socket connectionSocket = welcomeSocket.accept();
-					DistributedSystemNode.receive(connectionSocket);
+		while(true) {
+			try {
+				Socket connectionSocket = joinSocket.accept();
+				DistributedSystemNode.receive(connectionSocket);
 
-					/* Send the peer data to the node that wants to join the distributed network */
-					
-					message = new Message(Tag.JOIN, peerData.toString());
-					DistributedSystemNode.send(connectionSocket, message);
-					
-					/* Update the peer data with the new IP address */
-					
-					//TODO REVIEW
-					peerData.addPeerIP(connectionSocket.getRemoteSocketAddress().toString());
-					Log.i(TAG, "Added " + connectionSocket.getRemoteSocketAddress().toString() + " to peer data");
-					
-					/* Send synchronization message */
-					
-					DistributedSystemNode.send(connectionSocket, new TimeSynchronizationMessage());
+				/* Send the peer data to the node that wants to join the distributed network */
+				
+				message = new Message(Tag.JOIN, peerData.toString());
+				DistributedSystemNode.send(connectionSocket, message);
+				
+				/* Update the peer data with the new IP address */
+				
+				//TODO REVIEW
+				peerData.addPeerIP(connectionSocket.getRemoteSocketAddress().toString());
+				Log.i(TAG, "Added " + connectionSocket.getRemoteSocketAddress().toString() + " to peer data");
+				
+				/* Send synchronization message */
+				
+				DistributedSystemNode.send(connectionSocket, new TimeSynchronizationMessage());
+			}
+			catch(IOException e) {
+				Log.e(TAG, "Error while communicating with a peer");
+				e.printStackTrace();
 			}
 		}
-		catch(IOException e) {
-			Log.e(TAG, "Error while communicating with a peer");
-			e.printStackTrace();
-		}
 	}
-
 }
