c2a252f8697e0f2e227d6749fbdfa890e07f2cf0
Panos Sakkos <panos.sakkos@gmail.com>
2013-04-06 02:03:44 +0300

    Fixed identation

--- src/di/kdd/smartmonitor/protocol/PeerNode.java
+++ src/di/kdd/smartmonitor/protocol/PeerNode.java
@@ -11,7 +11,7 @@ import di.kdd.smartmonitor.protocol.ISmartMonitor.Tag;
 
 public final class PeerNode extends DistributedSystemNode implements Runnable {
 	/* The socket that the peer holds in order to get command messages from the Master */
-	
+
 	private ServerSocket commandsServerSocket;
 
 	private TimeSynchronization timeSync = new TimeSynchronization();
@@ -23,17 +23,17 @@ public final class PeerNode extends DistributedSystemNode implements Runnable {
 	 * starts a thread that accepts commands from the Master
 	 * @param socket The connected to the Master node socket
 	 */
-	
+
 	public PeerNode(Socket socket) {		
 		Message message;
 
 		try {
 			/* Start command-serving thread */
-			
+
 			new Thread(this).start();
 
 			/* The Master node was found, send the JOIN message */
-			
+
 			message = new Message(Tag.JOIN);
 			send(socket, message);	
 		}
@@ -49,11 +49,11 @@ public final class PeerNode extends DistributedSystemNode implements Runnable {
 			}
 		}
 	}
-	
+
 	/***
 	 * Accepts a socket connection on the COMMAND_PORT and waits for commands from the Master
 	 */
-	
+
 	@Override
 	public void run() {
 		Socket masterSocket;
@@ -62,63 +62,63 @@ public final class PeerNode extends DistributedSystemNode implements Runnable {
 		android.os.Debug.waitForDebugger();
 
 		Log.i(TAG, "Command-serving thread was started");
-		
+
 		try {
 			commandsServerSocket = new ServerSocket(ISmartMonitor.COMMAND_PORT);		
 			commandsServerSocket.setReuseAddress(true);
 			masterSocket = commandsServerSocket.accept();
-						
+
 			Log.i(TAG, "Accepted command socket from " + masterSocket.getRemoteSocketAddress().toString());
 		}
 		catch(IOException e) {
 			Log.e(TAG, "Failed to accept command socket");
 			e.printStackTrace();
-			
+
 			return;
 		}
-		
+
 		/* Listen on MasterSocket for incoming commands from the Master */
-		
+
 		Log.i(TAG, "Listening for commands from the Master node");
-		
-			while(!this.isInterrupted()) {
-				try {		
-					Message message;
-				
+
+		while(!this.isInterrupted()) {
+			try {		
+				Message message;
+
+				message = receive(masterSocket);
+
+				switch(message.getTag()) {
+				case PEER_DATA:
+					Log.i(TAG, "Received PEER_DATA command");
+
 					message = receive(masterSocket);
-				
-					switch(message.getTag()) {
-					case PEER_DATA:
-						Log.i(TAG, "Received PEER_DATA command");
-						
-						message = receive(masterSocket);
-						peerData.addPeersFromMessage(message);					
-						break;
-					case SYNC:
-						Log.i(TAG, "Received SYNC command");
-						
-						timeSync.timeReference(Long.parseLong(message.getPayload()));
-						break;
-					case NEW_PEER:
-						Log.i(TAG, "Received NEW_PEER command");	
-	
-						peerData.addPeerIP(message.getPayload());
-						break;
-					case START_SAMPLING:
-						Log.i(TAG, "Received START_SAMPLING command");
-	
-						break;
-					case STOP_SAMPLING:
-						Log.i(TAG, "Received STOP_SAMPLING command");
-						
-						break;
-					case SEND_PEAKS:
-							Log.i(TAG, "Received SEND_PEAKS command");
-						break;
-					default:
-						Log.e(TAG, "Not implemented Tag handling: " + message.getTag().toString());
-						break;
-					}
+					peerData.addPeersFromMessage(message);					
+					break;
+				case SYNC:
+					Log.i(TAG, "Received SYNC command");
+
+					timeSync.timeReference(Long.parseLong(message.getPayload()));
+					break;
+				case NEW_PEER:
+					Log.i(TAG, "Received NEW_PEER command");	
+
+					peerData.addPeerIP(message.getPayload());
+					break;
+				case START_SAMPLING:
+					Log.i(TAG, "Received START_SAMPLING command");
+
+					break;
+				case STOP_SAMPLING:
+					Log.i(TAG, "Received STOP_SAMPLING command");
+
+					break;
+				case SEND_PEAKS:
+					Log.i(TAG, "Received SEND_PEAKS command");
+					break;
+				default:
+					Log.e(TAG, "Not implemented Tag handling: " + message.getTag().toString());
+					break;
+				}
 			}
 			catch(IOException e) {
 				Log.e(TAG, "Error while listening to commands from Master node");
@@ -137,7 +137,7 @@ public final class PeerNode extends DistributedSystemNode implements Runnable {
 		catch(IOException e) {			
 		}
 	}
-	
+
 	@Override
 	public boolean isMaster() {
 		return false;
