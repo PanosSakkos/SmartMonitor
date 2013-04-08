ea834a0b89b2a3b9dd9e6acce1f670bddb824e8d
Panos Sakkos <panos.sakkos@gmail.com>
2013-04-08 17:23:19 +0300

    Fixed information logs

--- src/di/kdd/smartmonitor/protocol/DistributedSystemNode.java
+++ src/di/kdd/smartmonitor/protocol/DistributedSystemNode.java
@@ -46,7 +46,7 @@ public abstract class DistributedSystemNode extends Thread {
 	 */
 	
 	protected static Message receive(Socket socket) throws IOException {
-		Log.i(TAG, "Receiving from " + socket.getRemoteSocketAddress());
+		Log.i(TAG, "Receiving from " + socket.getInetAddress());
 		
 		return new Message(new BufferedReader(new InputStreamReader(socket.getInputStream())));		
 	}
@@ -64,7 +64,7 @@ public abstract class DistributedSystemNode extends Thread {
 	protected static Message receive(Socket socket, Tag tag) throws IOException, TagException {
 		Message message;
 		
-		Log.i(TAG, "Receiving from " + socket.getRemoteSocketAddress() + "with desired Tag: " + tag.toString());
+		Log.i(TAG, "Receiving from " + socket.getInetAddress() + "with desired Tag: " + tag.toString());
 		
 		message = new Message(new BufferedReader(new InputStreamReader(socket.getInputStream())));		
 		
