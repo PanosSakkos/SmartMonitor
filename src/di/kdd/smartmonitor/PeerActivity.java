ab45765025a01824bedd91622459d1cd4c82bad8
Panos Sakkos <panos.sakkos@gmail.com>
2013-04-23 16:44:04 +0300

    Added connect and connect at buttons at PeerActivity

--- src/di/kdd/smartmonitor/PeerActivity.java
+++ src/di/kdd/smartmonitor/PeerActivity.java
@@ -3,6 +3,7 @@ package di.kdd.smartmonitor;
 import di.kdd.smart.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PeerActivity extends Activity{
 
@@ -10,6 +11,16 @@ public class PeerActivity extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.peeractivity);
+		
+	}
+	
+	/* Button handlers */
+	
+	public void connectAt(View _) {
+		
+	}
+	
+	public void connect(View _) {
 		
 	}
 }
